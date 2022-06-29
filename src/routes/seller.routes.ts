453c6a218ff157d50786cb52a4ed9e870ca9f9ca
Diego Ferreira <diferreira.program@gmail.com>
2022-06-29 01:27:43 -0300

    add route get by id

--- src/routes/seller.routes.ts
+++ src/routes/seller.routes.ts
@@ -29,4 +29,14 @@ sellerRoutes.get("/", async (req, res) => {
   }
 });
 
+sellerRoutes.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const seller = await sellerRepository.findOneBy({ id: parseInt(id) });
+    return res.status(200).json({ result: seller });
+  } catch (err) {
+    return res.status(400).json({ error: err });
+  }
+});
+
 export { sellerRoutes };
